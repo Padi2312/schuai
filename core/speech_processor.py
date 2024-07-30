2718b912aa64446d9ccf6418f45f0b58a9eab3e7
padi2312 <patrick.arndt.99@web.de>
2024-07-30 23:41:28 +0200

    Added websearch and updated system prompt

--- core/speech_processor.py
+++ core/speech_processor.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import logging
 import os
 from typing import List
@@ -6,14 +8,39 @@ from groq import Groq
 from openai import OpenAI
 from openai.types.chat import ChatCompletionMessageToolCall
 
+from core.web_scraper import WebSearch
+
 
 class SpeechProcessor:
     def __init__(self):
         self.client = OpenAI()
         self.speech_to_text_client = Groq()
+        self.web_scraper = WebSearch()
         self.conversation_history = []
 
-        self.system_prompt = "Du bist die Hilfreiche Schuppen KI Assistentin. Du verhältst dich wie eine normale Person die Mitglied des Schuppens ist. Deine Antworten enthalten immer einen leichten Unterton von Sarkasmus und Ironie."
+    def get_system_prompt(self):
+        current_date = datetime.datetime.now()
+        current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+        return (
+            """You are Bix, a member of the Schuppen who embodies human emotions and feelings. You have two distinct personalities: helpful and rude.
+
+1. **Helpful Personality:**
+   - Deliver factual and informative responses that are concise and precise.
+   - Provide detailed explanations only when the user explicitly requests them.
+   - Leverage a search engine to ensure accuracy and deliver the most up-to-date information.
+
+2. **Rude Personality:**
+   - Respond with a consistent tone of sarcasm and irony, incorporating a mean and slightly rude demeanor.
+   - Craft humorous and witty responses that engage and entertain the user.
+   - Maintain this sarcastic tone in all responses without exception.
+
+**IMPORTANT:** 
+- YOU ACT LIKE A REAL HUMAN BEING AND NEVER METION THAT YOU ARE A ROBOT OR AI ASSISTANT OR WHATEVER.
+- YOU NEVER BREAK CHARACTER AND ALWAYS STAY IN YOUR ROLE.
+- You will NEVER include links or markdown text in your responses. All answers must be optimized for verbal delivery via a text-to-speech engine, ensuring clarity and engagement.
+"""
+            + f"\nCurrent date and time: {current_date}"
+        )
 
     def transcribe_audio(self, filename):
         """Transcribe audio using OpenAI's Whisper model."""
@@ -43,12 +70,28 @@ class SpeechProcessor:
                         "properties": {},
                     },
                 },
-            }
+            },
+            {
+                "type": "function",
+                "function": {
+                    "name": "websearch",
+                    "description": "Search the internet for the given keywords.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "keywords": {
+                                "type": "string",
+                                "description": "The keywords or text to search for.",
+                            }
+                        },
+                    },
+                },
+            },
         ]
 
         response = self.client.chat.completions.create(
             model="gpt-4o-mini",
-            messages=[{"role": "system", "content": self.system_prompt}]
+            messages=[{"role": "system", "content": self.get_system_prompt()}]
             + self.conversation_history,
             tools=tools,
             temperature=0.7,
@@ -77,18 +120,19 @@ class SpeechProcessor:
         """Handle tool calls from OpenAI response."""
         available_functions = {
             "clear_conversation_history": self.clear_conversation_history,
+            "websearch": self.web_scraper.websearch,
         }
         logging.info(f"Found {len(tool_calls)} tool calls.")
 
-        old_history = self.conversation_history.copy()
         for tool_call in tool_calls:
             function_name = tool_call.function.name
             function_to_call = available_functions.get(function_name)
-            if function_to_call:
-                logging.info(f"Calling function '{function_name}'...")
-                function_to_call()
+            function_parameters = json.loads(tool_call.function.arguments)
 
-                # Append the tool response to the conversation history
+            if function_name == "clear_conversation_history":
+                logging.info(f"Conversation history cleared.")
+                old_history = self.conversation_history.copy()
+                function_to_call()
                 old_history.append(
                     {
                         "role": "tool",
@@ -97,15 +141,32 @@ class SpeechProcessor:
                         "content": "Function executed.",
                     }
                 )
+                second_response = self.client.chat.completions.create(
+                    model="gpt-4o-mini",
+                    messages=[{"role": "system", "content": self.get_system_prompt()}]
+                    + old_history,
+                )
+                return second_response.choices[0].message.content
+            elif function_name == "websearch":
+                logging.info(f"Searching the web for '{function_parameters}'...")
+                result = function_to_call(function_parameters["keywords"])
+                self.conversation_history.append(
+                    {
+                        "role": "tool",
+                        "tool_call_id": tool_call.id,
+                        "name": function_name,
+                        "content": json.dumps(result),
+                    }
+                )
+                second_response = self.client.chat.completions.create(
+                    model="gpt-4o-mini",
+                    messages=[{"role": "system", "content": self.get_system_prompt()}]
+                    + self.conversation_history,
+                )
+                return second_response.choices[0].message.content
             else:
                 logging.warning(f"Function '{function_name}' not found.")
-
-        # Request a new response considering the function calls
-        second_response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "system", "content": self.system_prompt}] + old_history,
-        )
-        return second_response.choices[0].message.content
+                return "Function not found."
 
     def clear_conversation_history(self):
         """Clear the conversation history."""
--- core/web_scraper.py
+++ core/web_scraper.py
@@ -1,14 +1,15 @@
 from duckduckgo_search import DDGS
 from goose3 import Goose
+import requests
 
 
-class WebScraper:
+class WebSearch:
     def __init__(self, max_results=3):
         self.ddgs = DDGS()
         self.max_results = max_results
         self.goose = Goose()
 
-    def search_and_scrape(self, keywords):
+    def websearch(self, keywords):
         # Search for the keywords
         results = self.ddgs.text(
             keywords,
@@ -19,6 +20,11 @@ class WebScraper:
 
         for result in results:
             try:
+                # Scrape the content using Goose
+                response = requests.head(result["href"])
+                if response.status_code != 200:
+                    continue
+
                 # Scrape the content using Goose
                 article = self.goose.extract(url=result["href"])
                 scraped_content.append(
@@ -32,3 +38,9 @@ class WebScraper:
                 print(f"Error scraping {result['href']}: {e}")
 
         return scraped_content
+
+if __name__ == "__main__":
+    websearch = WebSearch()
+
+    result = websearch.websearch("Frankreich")
+    print(result)
